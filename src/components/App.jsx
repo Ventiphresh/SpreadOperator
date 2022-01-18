505e68e6234a8ae880dfe48838149c74e3e5204a
Ventiphresh <84814146+Ventiphresh@users.noreply.github.com>
2022-01-17 20:05:59 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setInputText(value);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          value={inputText}
+          name="listItem"
+          type="text"
+          onChange={handleChange}
+        />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
